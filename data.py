ed38aa8cb9367daf452c1cc88b537c87a49729a2
Lucas <97752019+Lorrust@users.noreply.github.com>
2023-04-21 14:36:36 -0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,157 @@
+question_data = {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "Nintendo started out as a playing card manufacturer.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question":
+    "Amazon acquired Twitch in August 2014 for $970 million dollars.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "Faust is a playable character in 'Guilty Gear Xrd Revelator'.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "The game 'Battlefield 1' takes place during World War I.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question":
+    "'Metal Gear Solid 3: Snake Eater' takes place on Shadow Moses Island.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question": "Donkey Kong was originally set to be a Popeye arcade game.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "In 'Super Mario Bros.', the clouds and bushes have the same artwork and are just different colors.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "Solid Snake is actually a clone from the DNA of Big Boss in the Metal Gear Solid series` history.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question":
+    "In 'Team Fortress 2', the Medic will be credited for an assist if he heals a Spy that successfully saps a building.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "In Sonic the Hedgehog universe, Tails` real name is Miles Prower.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "hard",
+    "question":
+    "The names of Roxas`s Keyblades in Kingdom Hearts are 'Oathkeeper' and 'Oblivion'.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "In Pokémon Sun and Moon, a male Salandit can evolve to a Salazzle.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "In Rocket League, you can play Basketball.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "Valve`s 'Portal' and 'Half-Life' franchises exist within the same in-game universe.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question":
+    "In the video game 'Transistor', 'Red' is the name of the main character.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question":
+    "In 'Call Of Duty: World At War', the first appearance of the 'PPSH-41' in 'Zombies' was in the map 'Der Riese'.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "In Resident Evil 4, the Chicago Typewriter has infinite ammo.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question":
+    "In 'Resident Evil', only Chris has access to the grenade launcher.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question": "The shotgun appears in every numbered Resident Evil game.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Entertainment: Video Games",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question":
+    "In 'Space Station 13', the station has a clown aboard it.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}
